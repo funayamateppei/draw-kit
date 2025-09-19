7c52dfdc548761a08f6d44eba669a9cfe7475cad
funayamateppei <t.funayama0217@gmail.com>
2025-09-19 23:20:53 +0900

    feat: DrawKitコンポーネントを追加し、index.cssをインポート

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import {StrictMode} from "react"
 import {createRoot} from "react-dom/client"
+import {DrawKit} from "./pages"
+import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>hoge</div>
+    <DrawKit />
   </StrictMode>,
 )
---
+++ src/pages/DrawKit.tsx
@@ -0,0 +1,3 @@
+export function DrawKit() {
+  return <div>DrawKit</div>
+}
